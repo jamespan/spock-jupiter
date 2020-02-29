84016841e528057e67b46b409a97d39210202773
jamespan <panjiabang@gmail.com>
2020-02-29 20:49:35 +0800

    mix spock and junit 5

--- src/test/groovy/spock/TryoutSpec.groovy
+++ src/test/groovy/spock/TryoutSpec.groovy
@@ -1,13 +1,26 @@
-package spock;
+package spock
 
-import spock.lang.Specification;
+import org.junit.jupiter.api.parallel.Execution
+import org.junit.jupiter.api.parallel.ExecutionMode
+import spock.lang.Specification
 
-/**
- *
- */
 class TryoutSpec extends Specification {
     def 'demo spec'() {
         expect:
-            true
+        true
+    }
+
+
+    @Execution(ExecutionMode.CONCURRENT)
+    def 'parameterized spec'() {
+        expect:
+        println "$a, $b"
+        println Thread.currentThread().getName()
+        a + 1 == b
+
+        where:
+        a | b
+        1 | 2
+        3 | 4
     }
 }
--- src/test/java/junit/JavaTest.java
+++ src/test/java/junit/JavaTest.java
@@ -1,12 +1,35 @@
 package junit;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+@SuppressWarnings("UnstableApiUsage")
 class JavaTest {
+
+    private static RateLimiter limiter;
+
+    @BeforeAll
+    static void beforeAll() {
+        limiter = RateLimiter.create(1);
+    }
 
     @Test
     void basicTest() {
         Assertions.assertTrue(true);
+    }
+
+    @Execution(ExecutionMode.CONCURRENT)
+    @CsvSource({"1,2", "3,4"})
+    @ParameterizedTest
+    void param(int input, int expect) {
+        limiter.acquire();
+        System.out.println(Thread.currentThread());
+        Assertions.assertEquals(expect, input + 1);
     }
 }
